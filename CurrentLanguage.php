7a029dab2f60c8198400d29d490bb06bf647e2bd
Jakub Piasecki <jakub@piaseccy.pl>
2020-12-17 19:54:01 +0100

    Ensure Drupal is fully bootstrapped

--- CurrentLanguage.php
+++ CurrentLanguage.php
@@ -1,22 +1,28 @@
 <?php
 
-namespace Drush\Commands\current_language;
+namespace Drush\Commands\drush_current_language;
 
 use Consolidation\AnnotatedCommand\CommandData;
 use Drupal\language\ConfigurableLanguageManagerInterface;
 use Drush\Commands\DrushCommands;
+use Drush\Drush;
 
 /**
  * Ensures the current language is in use.
  */
-class CurrentLanguage extends DrushCommands {
+class CurrentLanguageCommands extends DrushCommands {
 
   /**
    * Ensure current language is set.
    *
    * @hook pre-command *
    */
-  public function validateCommand(CommandData $commandData) {
+  public function preCommand(CommandData $commandData) {
+    // Drupal must be fully bootstraped in order to use this validation.
+    $boot_manager = Drush::bootstrapManager();
+    if (!$boot_manager->hasBootstrapped(DRUSH_BOOTSTRAP_DRUPAL_FULL)) {
+      return;
+    }
     /** @var \Drupal\Core\Language\LanguageManagerInterface $languageManager */
     $languageManager = \Drupal::service('language_manager');
     /** @var \Drupal\language\LanguageNegotiatorInterface $negotiator */
